20d43f158418a3b7ee356f4713940d839e2bfb2d
wnsdnjs70 <wnsdnjs700@gmail.com>
2023-08-10 16:52:56 +0900

    Javascript day 4

--- javascript/day4/tab.js
+++ javascript/day4/tab.js
@@ -1,25 +1,21 @@
+const mytabs = document.querySelectorAll('.tabItem');
+const tabpanels = document.querySelectorAll('.tabpanel');
 
-let list = document.getElementsByClassName('tabItem');
-
-// console.log
-for(let item of list){
-
-    item.addEventListener('click',function(){
-        if(item.getAttribute('tabindex') == 1){
-            document.getElementById('tabpanel2').style.display='none';
-            document.getElementById('tabpanel1').style.display='block';
-            document.getElementById('tabpanel3').style.display='none';
-        }
-        else if(item.getAttribute('tabindex') == 2){
-            document.getElementById('tabpanel1').style.display='none';
-            document.getElementById('tabpanel2').style.display='block';
-            document.getElementById('tabpanel3').style.display='none';
-        }
-        else if(item.getAttribute('tabindex') == 3){
-            document.getElementById('tabpanel3').style.display='block';
-            document.getElementById('tabpanel2').style.display='none';
-            document.getElementById('tabpanel1').style.display='none';
-        }
-
-    });
+const init = function(){
+    let initli = mytabs[0].firstElementChild;
+    let initA = initli.parentElement.parentElement.getAttribute('aria-controls');
+    console.log(initA);
+    initli.classList.add('tabBtn');
+    initli.focus();
+    let initId = document.getElementById(initA);
+    console.log(initId);
+    initId.classList.add('visit');
 }
+init();
+
+mytabs.forEach(item => {
+    item.firstChild.addEventListener('focus',function(event){
+        event.preventDefault();
+        
+    })
+})
